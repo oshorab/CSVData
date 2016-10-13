5b36cb14e1b32988f9c5dbafcfdba23e97bc080d
oshorab971 <oshorab971@fhs.win.fuhsd.org>
2016-10-13 14:10:13 -0700

    Just for fun Commit

---
+++ src./CSVData.java
@@ -0,0 +1,40 @@
+/***
+ * This is a helper class to read CSV data.
+ * hello
+ * @author oshorab971
+ *
+ */
+public class CSVData {
+	private double[][] data;
+	private String[] columnNames;
+
+	public CSVData() {
+
+	}
+
+	public static CSVData readCSVFile(String filename, int numLinesToIgnore, String[] colNames) {
+		return null;
+	}
+	
+	public double[] getRow(int rowIndex) {
+		return null;
+	}
+	
+	public double[] getColumn(int colIndex) {
+		return null;
+	}
+	
+	public double[] getColumn(String name) {
+		return null;
+	}
+	
+	public double[][] getRows(int startIndex, int endIndex) {
+		return null;
+	}
+	
+	public double[][] getRows(int[] rowIndexes) {
+		return null;
+	}
+	
+	
+}
