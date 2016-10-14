3778b848bab00ba221e9adf2c73447fba730416d
oshorab971 <oshorab971@fhs.win.fuhsd.org>
2016-10-14 11:06:11 -0700

    added to methods

--- src./CSVData.java
+++ src./CSVData.java
@@ -8,16 +8,41 @@ public class CSVData {
 	private double[][] data;
 	private String[] columnNames;
 
-	public CSVData() {
+	public CSVData(String filepath, String[] columnNames, int startRow) {
+		 this.filePathToCSV = filepath;
 
-	}
+		 String dataString = readFileAsString(filepath);
+		 String[] lines = dataString.split("\n");
 
+		 // number of data points
+		 int n = lines.length - startRow;
+		 this.numRows = n;
+		 int numColumns = columnNames.length;
+
+		 // create storage for column names
+		 this.columnNames = columnNames;
+
+		 // create storage for data
+		 this.data = new double[n][numColumns];
+		 for (int i = 0; i < lines.length - startRow; i++) {
+		 String line = lines[startRow + i];
+		 String[] coords = line.split(",");
+		 for (int j = 0; j < numColumns; j++) {
+		 if (coords[j].endsWith("#")) coords[j] = coords[j].substring(0, coords[j].length()-1);
+		 double val = Double.parseDouble(coords[j]);
+		 data[i][j] = val;
+		 }
+		 }
+		 }
 	public static CSVData readCSVFile(String filename, int numLinesToIgnore, String[] colNames) {
+		
+		
 		return null;
 	}
 	
 	public double[] getRow(int rowIndex) {
-		return null;
+		
+		return this.data[rowIndex];
 	}
 	
 	public double[] getColumn(int colIndex) {
